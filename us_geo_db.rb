012958e33c434c4aaa807d02ef7162ad77ba872a
Fred <frouds@gmail.com>
2014-03-24 21:17:03 -0500

    put file into lib

--- us_geo_db.rb
+++
@@ -1,9 +0,0 @@
-require 'active_record'
-require 'mysql2'
-
-ActiveRecord::Base.establish_connection(
-  :adapter  => 'mysql2',
-  :database => 'test',
-  :username => 'root',
-  :password => 'frouds',
-  :host     => 'localhost')
